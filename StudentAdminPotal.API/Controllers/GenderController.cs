e10e7ae49b8b90c2fce0b34ee14263758d8ad391
Villsource <anirut1800@gmail.com>
2023-02-18 13:07:16 +0700

    init project

---
+++ StudentAdminPotal.API/Controllers/GenderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentAdminPotal.API.DomainModel;
+
+namespace StudentAdminPotal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenderController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetAllGender()
+        {
+            List<Gender> genderList = new()
+            {
+                new Gender{Id=Guid.NewGuid(), Description= "Male"},
+                new Gender{Id=Guid.NewGuid(), Description="Female"},
+            };
+            return Ok(genderList);
+        }
+    }
+}
